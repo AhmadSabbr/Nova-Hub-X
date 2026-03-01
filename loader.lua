c4383fe909858b32582194a70ff28d86ad7b2039
Ahmad <marwanabram1234@gmail.com>
2026-03-01 03:31:59 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,7 +7,7 @@ local scripts = {
     [3101667897] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/LegendsOfSpeed.lua", -- Legends Of Speed
     [3623096087] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/MuscleLegends.lua", -- Muscle Legends
 	[131623223084840] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/EscapeTsunamiForBrainrot.lua", -- Escape Tsunami For Brainrots
-	[119579217517090] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/EscapeWavesForLuckyBlock.lua" -- Escape Tsunami For Lucky Blocks
+	[119579217517090] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/EscapeWavesForLuckyBlock.lua" -- Escape Waves For Lucky Blocks
 }
 
 local url = scripts[placeId]
