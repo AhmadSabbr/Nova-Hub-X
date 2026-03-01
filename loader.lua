de7928ff7f5b45b538b7e34ff94ce794b2842693
Ahmad <marwanabram1234@gmail.com>
2026-03-01 03:31:40 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,10 +4,10 @@ local HttpService = game:GetService("HttpService")
 local placeId = game.PlaceId
 
 local scripts = {
-    [3101667897] = "https://api.luarmor.net/files/v4/loaders/8ef3f808503edde6a153f8d96b3399de.lua", -- Legends Of Speed
-    [3623096087] = "https://api.luarmor.net/files/v4/loaders/0af0f34c0584457a069b941a757083a5.lua", -- Muscle Legends
-	[131623223084840] = "https://api.luarmor.net/files/v4/loaders/30e0bdc2688ee7554cc2a382b0964672.lua", -- Escape Tsunami For Brainrots
-	[119579217517090] = "https://api.luarmor.net/files/v4/loaders/9ccb5de30b7b1cdfb07a54e9ea5df6e3.lua" -- Escape Tsunami For Lucky Blocks
+    [3101667897] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/LegendsOfSpeed.lua", -- Legends Of Speed
+    [3623096087] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/MuscleLegends.lua", -- Muscle Legends
+	[131623223084840] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/EscapeTsunamiForBrainrot.lua", -- Escape Tsunami For Brainrots
+	[119579217517090] = "https://raw.githubusercontent.com/AhmadSabbr/Games/refs/heads/main/EscapeWavesForLuckyBlock.lua" -- Escape Tsunami For Lucky Blocks
 }
 
 local url = scripts[placeId]
